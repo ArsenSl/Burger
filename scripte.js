5df4b5bbcdf9726a93e9c0c9a78f4829aa94f218
ArsenSl <arsen15.olx@gmail.com>
2023-02-03 18:36:08 +0200

    twocommit

---
+++ scripte.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.wnyz');
+let menu = document.querySelector('.burger');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('vud');
+})
